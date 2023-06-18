b05551413aee8b8afe60d3e129600b059ec0b1d6
Black Smith-JD-40 <ol.channel.t@gmail.com>
2023-06-18 10:41:37 +0400

    Merged into main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,11 +11,9 @@ import java.util.concurrent.ThreadPoolExecutor;
 
 public class Main {
     public static void main(String[] args) {
-
         final var server = new HttpServer();
         ThreadPoolExecutor threadPoolExecutor = (ThreadPoolExecutor) Executors.newFixedThreadPool(64);
         server.go();
-
     }
 }
 
